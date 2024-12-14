fcacf81cf522b7300d8cb0603ed2c93ddd2aa230
ianfefe <felix.ian@estudante.ufjf.br>
2024-12-14 18:05:21 -0300

    atualiza app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import './App.css'
-import Filme, { FilmeProps } from "./Filme"
+import Filme from "./Filme"
+import Filtro from "./Filtro"
+import NavBar from "./NavBar"
+import Footer from './Footer'
 
 export default function App() {
 
   return (
     <>
-      <Filme titulo='a' descricao='b' duracao={10} classificacao={18}  genero={["comedia", "terror"]} imagem='https://a-static.mlcdn.com.br/1500x1500/poster-cartaz-moana-2-a-pop-arte-poster/poparteskins2/pos-03867-60x90cm/ed698c1aed8d6bee90cb3fb6fabd53ad.jpeg'>
-      </Filme>
+    <header>
+      <NavBar></NavBar>
+    </header>
+      <Filtro>
+        <Filme titulo='a' descricao='b' duracao={10} classificacao={18}  genero={["comedia", "terror"]} imagem='https://a-static.mlcdn.com.br/1500x1500/poster-cartaz-moana-2-a-pop-arte-poster/poparteskins2/pos-03867-60x90cm/ed698c1aed8d6bee90cb3fb6fabd53ad.jpeg'></Filme>
+      </Filtro>
+      <Filtro>
+        <Filme titulo='a' descricao='b' duracao={10} classificacao={18}  genero={["comedia", "terror"]} imagem='https://a-static.mlcdn.com.br/1500x1500/poster-cartaz-moana-2-a-pop-arte-poster/poparteskins2/pos-03867-60x90cm/ed698c1aed8d6bee90cb3fb6fabd53ad.jpeg'></Filme>
+      </Filtro>
+      <footer>
+        <Footer></Footer>
+      </footer>
     </>
   )
-}
+} 
