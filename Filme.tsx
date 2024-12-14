c81082c182b14e5036eed742777da0971764f9f0
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2024-12-14 17:35:19 -0300

    add interface filmes

---
+++ Filme.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+export interface FilmeProps {
+    titulo: string;
+    descricao: string;
+    duracao: number;
+    classificacao: number;
+    genero: string[];
+    imagem: string;
+}
+
+export default function Filme(filme: FilmeProps) {
+    return (
+        <>
+        <div className="filme">
+            <div className="titulo">{filme.titulo}</div>
+            <div className="descricao">{filme.descricao}</div>
+            <div className="duracao">{filme.duracao}</div>
+            <div className="classificacao">{filme.classificacao}</div>
+            <img className="imagem" src={filme.imagem} alt={filme.titulo}></img>
+            <div className="genero">{filme.genero}</div>
+        </div>
+        </>
+    )
+}
