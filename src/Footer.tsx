eecf3a17c1dc544afa30230d13254c33cd5bcc7d
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2024-12-14 18:02:31 -0300

    add footer

---
+++ src/Footer.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-links">
+          <a>Perguntas Frequentes</a>
+          <a>Central de Ajuda</a>
+          <a>Conta</a>
+          <a>Imprensa</a>
+          <a>Relações com Investidores</a>
+          <a>Carreiras</a>
+          <a>Formas de Assistir</a>
+          <a>Termos de Uso</a>
+          <a>Privacidade</a>
+          <a>Preferências de Cookies</a>
+          <a>Informações Corporativas</a>
+          <a>Entre em Contato</a>
+        </div>
+        <div className="footer-copyright">
+          <p>&copy; 2024 Isaac Ian. Todos os direitos reservados.</p>
+        </div>
+      </div>
+    </footer>
+  )
+}
