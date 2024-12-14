b39db18292887f9a97e423a8d387c183afe46ba7
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2024-12-14 17:36:54 -0300

    chamando componente filme na app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import React from 'react'
 import './App.css'
+import Filme, { FilmeProps } from "./Filme"
 
 function App() {
 
   return (
     <>
+      <Filme titulo='a' descricao='b' duracao={10} classificacao={18}  genero={["comedia", "terror"]} imagem='https://a-static.mlcdn.com.br/1500x1500/poster-cartaz-moana-2-a-pop-arte-poster/poparteskins2/pos-03867-60x90cm/ed698c1aed8d6bee90cb3fb6fabd53ad.jpeg'>
+      </Filme>
     </>
   )
 }
-
-export default App
