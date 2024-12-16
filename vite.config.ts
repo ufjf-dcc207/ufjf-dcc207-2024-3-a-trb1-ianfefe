3627409001acca8723bb4f8d67aab9ac2c16412c
ianfefe <felix.ian@estudante.ufjf.br>
2024-12-16 18:43:40 -0300

    atualiza base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ufjf-dcc207-2024-3-a-trb1-ianfefe.git"
+  base: "/ufjf-dcc207-2024-3-a-trb1-ianfefe"
 })
